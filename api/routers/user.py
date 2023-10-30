2d317200e45a1da7bdb98214843ecadf066adfa8
PantojaRodrigo <rodrigopanv@outlook.com>
2023-10-30 16:19:39 -0600

    Users update and delete router

--- api/routers/user.py
+++ api/routers/user.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi import status
 
-from api.schemas.user import LoginCredentials, LoginCredentialsResponse, CreateUserRequest
+from api.schemas.user import User, LoginCredentials, LoginCredentialsResponse, CreateUserRequest
 from api.services.user import UserService
 
 
@@ -17,9 +17,31 @@ def login(credentials:LoginCredentials):
         return JSONResponse({"message": "Invalid credentials"}, status_code=status.HTTP_401_UNAUTHORIZED)
     return json_encoder(user_credentials)
 
-@user_router.post("/user")
+@user_router.get("/users/{id}")
+def read_user_details(id):
+    user = UserService().get_user_details(id)
+    if not user:
+        return JSONResponse({"message": "No user found"}, status_code=status.HTTP_404_NOT_FOUND)
+    return json_encoder(user)
+    
+
+@user_router.post("/users")
 def create_new_user(createUserRequest : CreateUserRequest):
     response = UserService().create_user(createUserRequest)
     if not response:
         return JSONResponse({"message": "User could not be added"}, status_code=status.HTTP_409_CONFLICT)
     return JSONResponse({"message": "User added successfully"}, status_code=status.HTTP_201_CREATED)
+
+@user_router.put("/users/{id}")
+def update_patient(id: str, user: User):
+    response = UserService().update_user(id, user)
+    if not response:
+        return JSONResponse({"message": "User could not be updated"}, status_code=status.HTTP_409_CONFLICT)
+    return JSONResponse({"message": "User updated successfully"}, status_code=status.HTTP_200_OK)
+
+@user_router.delete("/users/{id}")
+def delete_patient(id: str):
+    response = UserService().delete_user(id)
+    if not response:
+        return JSONResponse({"message": "User could not be deleted"}, status_code=status.HTTP_409_CONFLICT)
+    return JSONResponse({"message": "User deleted successfully"}, status_code=status.HTTP_200_OK)
--- api/services/user.py
+++ api/services/user.py
@@ -2,6 +2,7 @@ from api.schemas.user import User,LoginCredentials, LoginCredentialsResponse, Us
 from api.utils.jwt_manager import create_token
 from config.mongoCon import MongoCon
 from api.utils import encrypt
+from bson import ObjectId
 
 class UserService():
     def login(self, credentials:LoginCredentials) -> LoginCredentialsResponse:
@@ -21,6 +22,13 @@ class UserService():
         token = create_token(dict(user_credentials))
         return LoginCredentialsResponse(app_token=token, user_credentials=user_credentials, role=role)
     
+    def get_user_details(self,id:str):
+        with MongoCon() as cnx:
+            user = cnx.users.find_one({"_id": ObjectId(id)},{"password":0})
+            if not user:
+                return None
+            return user
+    
     def create_user(self, create_user_request: CreateUserRequest):
         if create_user_request.password != create_user_request.confirm_password:
                 return None
@@ -39,3 +47,18 @@ class UserService():
                 return None
             return response
         
+    def update_user(self, id: str, user: User):
+        print(user.dict())
+        with MongoCon() as cnx:
+            response = cnx.users.find_one_and_update({"_id": ObjectId(id)}, {"$set": user.dict()})
+            print(response)
+            if not response:
+                return None
+            return response
+    
+    def delete_user(self, id: str):
+        with MongoCon() as cnx:
+            response = cnx.users.find_one_and_delete({"_id": ObjectId(id)})
+            if not response:
+                return None
+            return response
