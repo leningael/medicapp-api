338f660e0ef3aaaaa04a49d306fe11b7c7a87a62
PabloR9080 <a19216279@alumnos.uady.mx>
2023-11-15 23:35:54 -0600

    chore(notes): add crud operations to service

--- api/routers/notes.py
+++ api/routers/notes.py
@@ -3,18 +3,36 @@ from fastapi import APIRouter
 from fastapi import status
 from fastapi import HTTPException
 from fastapi import Depends
+from fastapi.encoders import jsonable_encoder
 
 
 from api.services.notes import NotesService
-from api.schemas.notes import Notes
+from api.schemas.notes import NoteContent, NoteOverview, Note
 
 notes_router = APIRouter(tags=["notes"],prefix='/note')
 
 @notes_router.get("/")
 def index():
-    notes = NotesService.get_notes()
-    #if not notes:
-    #    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Notes not found")
-    return notes
+    notes = NotesService().get_notes()
+    return jsonable_encoder(notes)
 
+@notes_router.post("/")
+def create_note(note: Note):
+    response = NotesService().post_note(note)
+    return response
 
+@notes_router.get("/{id}")
+def delete_note(id: str):
+    response = NotesService().get_details(id)
+    content = response.pop('content', False)
+    return jsonable_encoder({**response, **content})
+
+@notes_router.delete("/{id}")
+def delete_note(id: str):
+    response = NotesService().delete_note(id)
+    return response
+
+@notes_router.put("/{id}")
+def update_note(id: str, note: Note):
+    response = NotesService().update_note(id, note)
+    return response
--- api/schemas/notes.py
+++ api/schemas/notes.py
@@ -1,23 +1,40 @@
-from pydantic import BaseModel
-from typing import Optional
+import datetime
 
-class Notes(BaseModel):
-    ...
+from pydantic import BaseModel, Field
+from typing import List, Optional, Union
+from api.schemas.patient import PatientOverview
 
 class NoteOverview(BaseModel):
-    _id: str
-    patientName: str
-    reason: str
-    startDate: str
-    endDate: str
+    id: Union[str, None] = Field(default=None,alias='_id')
+    patient: Union[PatientOverview,None] = None
+    reason: Union[str,None] = None
+    date: Union[datetime.datetime, None] = None
+
+class Medicament(BaseModel):
+    id: Union[str, None] = Field(default=None,alias='_id')
+    medicament: str
+    quantity: str
+    consume_method: str
+    frequency: str
+    duration: str
+    notes: str
 
 class NoteContent(BaseModel):
-    _id: str
-    reason: Optional[str]
+    id: Union[str,None] = Field(default=None,alias='_id')
+    reason: Union[str,None] = None
     diagnosis: str
     temperature: str
     weight: str
     height: str
     imc: int
-    systolicPressure: str
-    diastolicPressure: str
+    sistolic_pressure: str
+    diastolic_pressure: str
+    medication: Union[List[Medicament], None] = None
+    notes: Union[str, None] = None
+    
+class Note(BaseModel):
+    id: Union[str, None] = Field(default=None,alias='_id')
+    patient: PatientOverview
+    doctor_name: str
+    date: Union[datetime.datetime, None]  = Field(default=datetime.datetime.now())
+    content: NoteContent
